b4d858ba17da37db7117462378994a0260d22bfc
fAyr08 <ayrj085@gmail.com>
2023-08-21 02:10:28 +0530

    Inception

---
+++ app.js
@@ -0,0 +1,50 @@
+// /*
+// <div id="parent">
+//     <div id="child">
+//         <h1>I'm a h1 tag</h1>
+//     </div>
+// </div>
+// */
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I'm a h1 tag")
+//   )
+// );
+
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I'm a h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [ React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag"),
+    ]
+    )
+  );
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+console.log(heading); //This is an object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading); //This render method converts the h1 object to the h1 tag
+
+root.render(parent);
